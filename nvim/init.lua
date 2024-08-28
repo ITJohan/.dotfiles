0ed662b64b26d0dfc1a8c5f47b2177164e748528
Johan Lindkvist <johanlindkvist89@gmail.com>
2024-08-28 15:52:37 +0200

    init floq func

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,7 +39,6 @@ local lsp_zero = require('lsp-zero')
 local lsp_attach = function(client, bufnr)
   local opts = {buffer = bufnr}
 
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
   vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
   vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
   vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
@@ -85,6 +84,16 @@ cmp.setup({
 ---
 
 -- vim.g.mapleader = " " -- sets leader key to space
+vim.g.netrw_banner = 0 -- gets rid of annoying banner for netrw
+vim.g.netrw_browse_split = 0 -- open in prior window
+vim.g.netrw_altv = 1 -- change from left splitting to right splitting
+vim.g.netrw_liststyle = 3 -- tree style view in netrw
+vim.opt.title = true -- show title
+vim.opt.path:append('**') -- enable "fuzzy find" in command mode
+vim.opt.wildignore = '*/node_modules/*' -- ignore in "fuzzy find"
+vim.opt.backup = false -- disable creation of backup files
+vim.opt.number = true -- turn on line numbers
+vim.opt.relativenumber = true -- show relative line numbers
 
 -- Functional wrapper for mapping custom keybindings
 function map(mode, lhs, rhs, opts)
@@ -96,20 +105,44 @@ function map(mode, lhs, rhs, opts)
 end
 
 -- My own settings
-vim.opt.relativenumber = true -- show relative line numbers
-vim.opt.path:append('**') -- enable "fuzzy find" in command mode
-vim.opt.wildignore = '*/node_modules/*' -- ignore in "fuzzy find"
 vim.opt.expandtab = true -- use spaces instead of tabs
 vim.opt.tabstop = 2 -- use two spaces
 vim.opt.shiftwidth = 2 -- use two spaces for auto-ident
 vim.opt.splitright = true -- split windows to the right
 vim.opt.guicursor = 'a:blinkon100' -- enable blinking cursor
 
-function Split() -- function for splitting into four windows
+function InitSplit() -- function for splitting into four windows
   vim.cmd('vnew')
   vim.cmd('bo term')
   vim.cmd('vert term')
   vim.cmd('resize 20')
+end
+
+function InitFloq()
+  vim.cmd(":bo term")
+
+  vim.cmd(":vert term")
+  vim.cmd("startinsert")
+  vim.api.nvim_input("cd ../floq-db && docker compose up<CR>")
+  vim.api.nvim_input('<C-\\><C-n>')
+
+  vim.defer_fn(function()
+    vim.cmd(":vert term")
+    vim.cmd("startinsert")
+    vim.api.nvim_input("npm start<CR>")
+    vim.api.nvim_input('<C-\\><C-n>')
+    vim.api.nvim_input('gg')
+
+    vim.defer_fn(function()
+      vim.cmd(":vert term")
+      vim.cmd("startinsert")
+      vim.api.nvim_input("cd ../floq && npm start<CR>")
+      vim.api.nvim_input('<C-\\><C-n>')
+      vim.api.nvim_input('gg')
+      vim.cmd(":resize 20")
+      vim.api.nvim_input("<C-w>k")
+    end, 100)
+  end, 100)
 end
 
 -- Get clipboard to work in Crostini
